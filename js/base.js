26b39e975ddd33eaf281a8557eff220e86e581ef
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-03-01 22:45:58 +0300

    my-modal

--- js/base.js
+++ js/base.js
@@ -1,2 +1,1 @@
-const $ = {};
-window.$ = $;
+window.$ = {};
--- js/plugins/modal.js
+++ js/plugins/modal.js
@@ -1,7 +1,39 @@
+function _createModal(options) {
+    const myModal = document.createElement('div')
+    myModal.classList.add('my-modal')
+    myModal.insertAdjacentHTML('afterbegin', `
+        <div class="my-modal-overlay">
+            <div class="my-modal-window">
+                <span class="my-modal-close"></span>
+                <div class="my-modal-container">
+                    <div class="my-modal-head">
+                        <p class="my-modal-head__title">Some title</p>
+                    </div>
+                    <div class="my-modal-body">
+                        <p>Lorem ipsum dolor.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, hic.</p>
+                    </div>
+                    <div class="my-modal-footer">
+                        <button class="button">Ok</button>
+                        <button class="button">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+
+    document.body.appendChild(myModal);
+    return myModal;
+}
+
 $.modal = function (options) {
+    const $myModal = _createModal(options);
     return {
-        open() {},
-        close() {},
-        destroy() {},
+        open() {
+        },
+        close() {
+        },
+        destroy() {
+        }
     }
-};
+}
